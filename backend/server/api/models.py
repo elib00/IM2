09337d23f3df4828a0dfa44d0b5d1026e352d6d8
elib00 <joshuanapinas19@gmail.com>
2024-08-22 17:39:20 +0800

    [Joshua] Added migrations

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(
+        default=0,
+        null=False
+    )
+
+    gender = models.CharField(max_length=50, default="Others")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return super().__str__()
+    
+class Account(models.Model):
+    email = models.EmailField()
+    username = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
+    
+    
