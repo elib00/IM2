6d26c63e53c571a6e675492964f8d7202ac72818
elib00 <joshuanapinas19@gmail.com>
2024-08-22 17:10:25 +0800

    Added models

--- backend/server/api/views.py
+++ backend/server/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(http_method_names=["GET", "POST"])
+def homepage(request: Request):
+    res = {"message": "Hi"}
+    return Response(data=res, status=status.HTTP_200_OK)
