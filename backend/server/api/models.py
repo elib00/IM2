d3124c439a008d99e560e7ac11dcdc77a3383f26
elib00 <joshuanapinas19@gmail.com>
2024-08-22 19:12:42 +0800

    [Joshua] Added new models

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -3,25 +3,24 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    age = models.PositiveIntegerField(
-        default=0,
-        null=False
-    )
-
+    age = models.PositiveIntegerField(default=0, null=False)
     gender = models.CharField(max_length=50, default="Others")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    def __str__(self) -> str:
-        return super().__str__()
-    
 class Account(models.Model):
-    email = models.EmailField()
-    username = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return super().__str__()
-    
-    
+class Movie(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    actors = models.TextField()
+    director = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField(default=0)    
+
+class MovieSession(models.Model):
+    pass
