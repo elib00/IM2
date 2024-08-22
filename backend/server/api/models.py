396f2b6efc8158ec28c699b32f12f960d1066044
elib00 <joshuanapinas19@gmail.com>
2024-08-22 22:37:19 +0800

    [Joshua] Added migrations

--- backend/server/api/models.py
+++ backend/server/api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    age = models.PositiveIntegerField(default=0, null=False)
-    gender = models.CharField(max_length=50, default="Others")
+    age = models.PositiveIntegerField(null=False, default=0)
+    gender = models.CharField(max_length=50, default="Other")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
