3b7ab0c291402b8e7158c02d9d0e13f771df55d6
elib00 <joshuanapinas19@gmail.com>
2024-08-25 22:12:12 +0800

    [Joshua] Added movie model

---
+++ backend/server/movies/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class Movie(models.Model):
+    movie_name = models.CharField(max_length=50)
+    director = models.CharField(max_length=50)
+    producer = models.CharField(max_length=50)
+    synopsis = models.TextField()
+    cast = models.TextField()
+    duration = models.DurationField()
+    movie_format = models.CharField(
+        max_length=3, 
+        choices=[
+            ("2D", "2D"),
+            ("3D", "3D")
+        ], 
+        default="2D"
+    )
+    cinema_type = models.CharField(
+        max_length=20,
+        choices=[
+            ("IMAX", "IMAX"),
+            (" Director's Club", "Director's Club"),
+            ("Regular", "Regular")
+        ],
+        default="Regular"
+    )
+    date_and_time = models.DateTimeField()
+    film_rating = models.CharField(
+        max_length=10,
+        choices=[
+            ("G", "General Audience"),
+            ("PG", "Parental Guidance"),
+            ("R-13", "Restricted-13"),
+            ("R-16", "Restricted-16"),
+            ("R-18", "Restricted-18")
+        ],
+        default="G"
+    )
+    genre = models.CharField(max_length=100)
+    
+    
+
+    
+    
