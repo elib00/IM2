a76551096fff5cc7ddcf7e1fa2d6cadd6dbb5ad8
elib00 <joshuanapinas19@gmail.com>
2024-08-27 23:48:53 +0800

    [Joshua] Migrated db

--- backend/server/movies/views.py
+++ backend/server/movies/views.py
@@ -1,5 +1,4 @@
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
 from .models import Movie
